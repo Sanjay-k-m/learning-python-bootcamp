7c59cdec517b7e294140e2520bf2661aeb51396d
sanjay-k-m <sanjay_k.m@outlook.com>
2025-07-20 09:43:19 +0530

    Day 18
    packages using

---
+++ Day 18/main.py
@@ -0,0 +1,18 @@
+from turtle import Turtle,Screen
+# from turtle import Turtle as t
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("green")
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+
+my_screen = Screen()
+my_screen.exitonclick()
