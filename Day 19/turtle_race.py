2cb26b0b63b027097ad80ad0d26ce8e6e7e9d681
sanjay-k-m <sanjay_k.m@outlook.com>
2025-08-13 10:10:31 +0530

    day-19 trurtle race completed

--- Day 19/turtle_race.py
+++ Day 19/turtle_race.py
@@ -3,7 +3,7 @@ from turtle import  Turtle,Screen
 
 screen = Screen()
 screen.setup(width=500,height=400)
-user_bet = screen.textinput(title="Make your bet",prompt="Which turtle will win the rece? Enter a color : ")
+user_bet = screen.textinput(title="Make your bet",prompt="Which turtle will win the race? Enter a color : ")
 colors = ["red","orange","yellow","green","blue","purple"]
 y_positions = [-70,-40,-10,20,50,80]
 
